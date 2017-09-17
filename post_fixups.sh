c19223637eacc87594738f2abd64f419529e2cf8
Vescio, Angelo Robert <vesh@gatech.edu>
2017-09-16 21:48:11 -0500

    changing package.json

--- post_fixups.sh
+++ post_fixups.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#cp networks.js node_modules/bitcore-lib/lib/
 find . -type f -not -path "./post_fixups.sh*" -exec sed -i 's#http://127.0.0.1:3232#http://bws-1.twocatus.org:3232#g' {} +
 find . -type f -not -path "./post_fixups.sh*" -exec sed -i 's/BTC/DTC/g' {} +
 find . -type f -not -path "./post_fixups.sh*" -exec sed -i 's/Bitcoin/Ducatuscoin/g' {} +
