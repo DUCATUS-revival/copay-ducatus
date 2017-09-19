c1a146724dcf2d21c4ca1823e8c7bdb415eb93eb
Noah Hunter <nhhntr11@gmail.com>
2017-09-19 20:21:50 +0000

    Changed the domain over and now using https

--- post_fixups.sh
+++ post_fixups.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find . -type f -not -path "./post_fixups.sh*" -exec sed -i 's#http://127.0.0.1:3232#http://bws-1.twocatus.org:3232#g' {} +
-find . -type f -not -path "./post_fixups.sh*" -exec sed -i 's/BTC/DTC/g' {} +
-find . -type f -not -path "./post_fixups.sh*" -exec sed -i 's/Bitcoin/Ducatuscoin/g' {} +
-find . -type f -not -path "./post_fixups.sh*" -exec sed -i 's/bitcoin/ducatuscoin/g' {} +
+find . -type f -not -path "./post_fixups.sh*" -not -path ".git/*" -exec sed -i 's#http://bws-1.twocatus.org:3232#https://dws.ducatus.io#g' {} +
+find . -type f -not -path "./post_fixups.sh*" -not -path ".git/*" -exec sed -i 's/BTC/DTC/g' {} +
+find . -type f -not -path "./post_fixups.sh*" -not -path ".git/*" -exec sed -i 's/Bitcoin/Ducatuscoin/g' {} +
+find . -type f -not -path "./post_fixups.sh*" -not -path ".git/*" -exec sed -i 's/bitcoin/ducatuscoin/g' {} +
